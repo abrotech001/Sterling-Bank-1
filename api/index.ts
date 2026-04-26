c0dfb9f3a2942200d5784802f3abdebdb77b9578
abrotech001 <abrotexh@googlemail.com>
2026-04-26 05:22:03 +0100

    Delete api/index.ts

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import app from "../artifacts/api-server/src/app.js";
-import { initTelegramBots } from "../artifacts/api-server/src/lib/telegram.js";
-
-// Initialize Telegram bots
-initTelegramBots("webhook").catch(() => {});
-
-// Vercel handles Express apps natively, so we just export the app directly!
-export default app;
